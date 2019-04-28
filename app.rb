6969614dd454fdfe939fe55df5544c8f1d16ab33
Joy Lin <vvlnote@hotmail.com>
2019-04-28 17:11:10 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -5,15 +5,15 @@ class App < Sinatra::Base
 	end
   
   get '/hello' do
-    
+    erb :hello
   end
   
   get '/goodbye' do
-    
+    erb :goodbye
   end
   
   get '/date' do
-    
+    erb :date
   end
 
 end
